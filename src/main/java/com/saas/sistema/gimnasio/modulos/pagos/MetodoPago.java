d28b1a1cb2550a034871eec33b6a6f7913700ba3
HoldenAndy <kaziroz09@gmail.com>
2026-03-22 22:33:47 -0500

    Creacion de ENUMs

---
+++ src/main/java/com/saas/sistema/gimnasio/modulos/pagos/MetodoPago.java
@@ -0,0 +1,9 @@
+package com.saas.sistema.gimnasio.modulos.pagos;
+
+public enum MetodoPago {
+    EFECTIVO,
+    TARJETA_CREDITO,
+    TARJETA_DEBITO,
+    TRANSFERENCIA,
+    YAPE
+}
---
+++ src/main/java/com/saas/sistema/gimnasio/modulos/suscripciones/EstadoSuscripcion.java
@@ -0,0 +1,8 @@
+package com.saas.sistema.gimnasio.modulos.suscripciones;
+
+public enum EstadoSuscripcion {
+    ACTIVA,
+    VENCIDA,
+    PENDIENTE_PAGO,
+    CANCELADA
+}
