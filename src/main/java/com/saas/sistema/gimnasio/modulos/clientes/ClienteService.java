f0ae5ff1d74e2aa5c895b66cafe2b00eac195d81
HoldenAndy <kaziroz09@gmail.com>
2026-04-05 15:38:58 -0500

    Ampliar metodo para obtener clientes, con filtro de fecha y estado activo o inactivo

--- src/main/java/com/saas/sistema/gimnasio/modulos/clientes/ClienteService.java
+++ src/main/java/com/saas/sistema/gimnasio/modulos/clientes/ClienteService.java
@@ -3,14 +3,17 @@ package com.saas.sistema.gimnasio.modulos.clientes;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import java.time.LocalDate;
 import java.util.UUID;
 
 public interface ClienteService {
     ClienteResponseDto crearCliente(ClienteRequestDto dto);
 
-    Page<ClienteResponseDto> obtenerTodosLosClientes(Pageable pageable);
-
+    Page<ClienteResponseDto> obtenerTodosLosClientes(String query, LocalDate inicio, LocalDate fin, boolean estadoActivo, Pageable pageable);
+    
     ClienteResponseDto obtenerClientePorId(UUID id);
+
+    void reactivarCliente(UUID id);
 
     ClienteResponseDto actualizarCliente(UUID id, ClienteRequestDto dto);
 
